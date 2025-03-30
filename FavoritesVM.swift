50ffc57822bc5332e4b6de37c234bda194626233
Geeck03 <aramaea.marie@gmail.com>
2025-03-29 21:25:46 -0500

    Files Named

---
+++ FavoritesVM.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesVM.swift
+//  Book_Search_App_CoreData
+//
+//  Created by Sewell, Aramaea on 3/29/25.
+//
+
+import Foundation
---
+++ SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Book_Search_App_CoreData
+//
+//  Created by Sewell, Aramaea on 3/29/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
