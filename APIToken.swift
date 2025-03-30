1046b0a8e3a3a8b4a04642796123280102f60d1c
Geeck03 <aramaea.marie@gmail.com>
2025-03-30 17:26:43 -0500

    Book Model created

---
+++ APIToken.swift
@@ -0,0 +1,8 @@
+//
+//  APIToken.swift
+//  Book_Search_App_CoreData
+//
+//  Created by Sewell, Aramaea on 3/30/25.
+//
+
+import Foundation
---
+++ NetworkManagerVM.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManagerVM.swift
+//  Book_Search_App_CoreData
+//
+//  Created by Sewell, Aramaea on 3/30/25.
+//
+
+import Foundation
