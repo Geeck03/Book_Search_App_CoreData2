a83b67f8e1134cd463f6a6b9230312c48ee44465
Geeck03 <aramaea.marie@gmail.com>
2025-04-02 21:03:30 -0500

    Lot's of progress

---
+++ DisplayView.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayView.swift
+//  Book_Search_App_CoreData
+//
+//  Created by Sewell, Aramaea on 4/2/25.
+//
+
+import SwiftUI
+
+struct DisplayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisplayView()
+}
